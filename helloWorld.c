56c3f121f9567b316adaaabbdaa0ef52635d65ee
U-PS14A12\vantan <vantan@PS14A12.(none)>
2015-01-28 03:07:20 +0900

    first  commit

---
+++ helloWorld.c
@@ -0,0 +1,14 @@
+	/* はじめてのc言語プログラム*/
+
+#include <stdio.h> /* printfを利用するために必要*/
+
+
+int main(void)
+{
+	int i = 0;
+	for (i = 0; i < 10; i++){
+		printf("hello World\n");		// 画面に表示
+	}
+	return 0;
+
+}
